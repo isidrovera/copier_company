894daa9b50eeeec3c0b39bd404c9913b23ce6801
isidrovera <118472736+isidrovera@users.noreply.github.com>
2023-06-21 20:34:25 -0500

    Update descargas.py

--- models/descargas.py
+++ models/descargas.py
@@ -6,19 +6,8 @@ class DescargaArchivos(models.Model):
     
     name = fields.Char(string='Nombre del archivo')
     url = fields.Char(string='URL del archivo')
-    prueba = fields.Html(string="Prueba")
+    modelo = fields.Char(string="Modelo")
     
     def descargar_archivo(self):
         url = self.url
-        response = requests.get(url, stream=True)
-        nombre_archivo = url.split("/")[-1]
-        headers = dict(response.headers)
-        tipo_contenido = headers.get("content-type")
-        tamano_archivo = headers.get("content-length")
-        if response.status_code == 200:
-            response.raw.decode_content = True
-            with open(nombre_archivo, "wb") as archivo:
-                shutil.copyfileobj(response.raw, archivo)
-            return "Archivo descargado exitosamente"
-        else:
-            return "Error al descargar el archivo"
+        
