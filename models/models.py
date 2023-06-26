cae41460bd0ce6b492ed3f7a2c4f0fb08988d5f2
isidrovera <118472736+isidrovera@users.noreply.github.com>
2023-06-26 16:49:25 -0500

    maquinas

--- models/models.py
+++ models/models.py
@@ -11,7 +11,7 @@ class copier_company(models.Model):
     name = fields.Many2one('modelos.maquinas',string='Maquina')
     
     serie_id = fields.Char(string='Serie', required=True)
-    marca_id = fields.Many2one('marcas.maquinas',string='Marca', required=True,)
+    marca_id = fields.Many2one('marcas.maquinas',string='Marca', required=True,related='name.marca_id')
     cliente_id = fields.Many2one('res.partner',string='Cliente', required=True, )
     
     _rec_name = 'serie_id'
