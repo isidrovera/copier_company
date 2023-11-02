d3f484efbf486511d77f9c33e89df8d6eeac30b8
isidrovera <118472736+isidrovera@users.noreply.github.com>
2023-11-02 16:22:54 -0500

    ...

--- models/modelos.py
+++ models/modelos.py
@@ -2,6 +2,9 @@ from odoo import models, fields, api
 
 class ModelosMaquinas(models.Model):
     _name = 'modelos.maquinas'
+    _sql_constraints = [
+        ('name_uniq', 'unique(name)', 'El modelo de la máquina debe ser único!')
+    ]
     name = fields.Char(string='Modelo')
     marca_id = fields.Many2one('marcas.maquinas',string='Marca')
     active = fields.Boolean(string="Activo", default=True)
