90fe1c01ac75ee6e48a307508660d786e3574d09
isidrovera <118472736+isidrovera@users.noreply.github.com>
2023-11-27 17:47:27 -0500

    Update models.py

--- models/models.py
+++ models/models.py
@@ -7,22 +7,21 @@ class copier_company(models.Model):
     _name = 'copier.company'
     _description = 'Aqui se veran los archivos de onedrive'
     _inherit = ['mail.thread', 'mail.activity.mixin']
-    _rec_name = 'serie_id'
+   
     name = fields.Many2one('modelos.maquinas',string='Maquina')
     
     serie_id = fields.Char(string='Serie', required=True)
     marca_id = fields.Many2one('marcas.maquinas',string='Marca', required=True,related='name.marca_id')
     cliente_id = fields.Many2one('res.partner',string='Cliente', required=True, )
     
-    def name_get(self):
-        result = []
+    @api.multi
+    def _rec_name(self):
+        result = super(copier_company, self)._rec_name()
         for record in self:
-            # Asumiendo que el modelo relacionado 'modelos.maquinas' tiene un campo 'display_name' o 'name'.
-            maquina_name = record.name.display_name if record.name else ''
-            serie = record.serie_id or ''
-            # Combina el nombre de la máquina y la serie para mostrar.
-            name_combined = f"[{maquina_name}] Serie: {serie}"
-            result.append((record.id, name_combined))
+            if record.serie_id:
+                result[record.id] = "{}({})".format(record.name.name, record.serie_id)
+            else:
+                result[record.id] = record.name
         return result
        
     def crear_ticket(self):
