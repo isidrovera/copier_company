42bc205e3c677930a40f524f5ce4bd32c0912a67
isidrovera <118472736+isidrovera@users.noreply.github.com>
2023-12-01 11:21:22 -0500

    Update controllers.py

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -5,14 +5,19 @@ class DescargaArchivosController(http.Controller):
     def descarga_archivos(self, **kw):
         partner = request.env.user.partner_id
         
-        stage = request.env['sale.order'].sudo().search([('subscription_state', '=', '3_progress')], limit=1)
-        if stage:
-            order = request.env['sale.order'].sudo().search([('partner_id', '=', partner.id), ('state', '=', stage.id)], limit=1)
-            if order:
-                docs = request.env['descarga.archivos'].search([])
-                return request.render('copier_company.client_portal_descarga_archivos', {'docs': docs})
+        # Supongamos que existe un campo "subscription_state" en el modelo "res.partner"
+        # que indica el estado de la suscripción del cliente.
+        subscription_state = partner.subscription_state
         
+        # Comprueba si el estado de la suscripción es "3_progress".
+        if subscription_state == '3_progress':
+            # Si el cliente tiene una suscripción en progreso, busca los documentos y muestra la página.
+            docs = request.env['descarga.archivos'].search([])
+            return request.render('copier_company.client_portal_descarga_archivos', {'docs': docs})
+        
+        # Si el cliente no tiene una suscripción en progreso, muestra un mensaje.
         return request.render('copier_company.no_subscription_message')
+
     
 class PortalAlquilerController(http.Controller):
     
