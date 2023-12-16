7e915577d3347268fbd9a1bb6b55e3b9eec007d8
isidrovera <118472736+isidrovera@users.noreply.github.com>
2023-12-16 10:04:21 -0500

    Update controllers.py

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,37 +1,29 @@
 from odoo import http
 from odoo.http import request
-
 class DescargaArchivosController(http.Controller):
-    @http.route('/descarga/archivos', type='http', website=True, auth='user')
-    def descarga_archivos(self, page=0, **kw):
+    @http.route('/descarga/archivos', type='http', website=True)
+    def descarga_archivos(self, **kw):
         partner = request.env.user.partner_id
-        limit = 10  # Número de registros por página
-        page = int(page) if page else 1
-        offset = (page - 1) * limit
 
+        # Buscar suscripciones del partner que estén en el estado '3_progress'
         subscriptions_in_progress = request.env['sale.order'].sudo().search([
             ('partner_id', '=', partner.id),
-            ('subscription_state', '=', '3_progress')
+            ('subscription_state', '=', '3_progress')  # Usando el valor del estado proporcionado
         ])
 
+        # Comprobar si hay suscripciones en el estado '3_progress'
         if subscriptions_in_progress:
-            doc_count = request.env['descarga.archivos'].sudo().search_count([])
-            docs = request.env['descarga.archivos'].sudo().search([], limit=limit, offset=offset)
-            pager = request.website.pager(
-                url="/descarga/archivos",
-                total=doc_count,
-                page=page,
-                step=limit
-            )
-            return request.render('copier_company.Descargas', {'docs': docs, 'pager': pager})
+            # Si existe al menos una suscripción en progreso, buscar los documentos relacionados
+            docs = request.env['descarga.archivos'].search([])
+            return request.render('copier_company.Descargas', {'docs': docs})
         else:
+            # Si no hay suscripciones en el estado '3_progress', mostrar un mensaje
             return request.render('copier_company.no_subscription_message')
 
-
     
-#class PortalAlquilerController(http.Controller):
+class PortalAlquilerController(http.Controller):
     
-   # @http.route('/portal/alquiler/<int:alquiler_id>', auth='public', website=True)
-   # def portal_alquiler_form(self, alquiler_id, **kwargs):
-      #  alquiler = request.env['cotizacion.alquiler'].sudo().browse(alquiler_id)
-      #  return request.render('copier_company.portal_alquiler_form', {'alquiler': alquiler})
+    @http.route('/portal/alquiler/<int:alquiler_id>', auth='public', website=True)
+    def portal_alquiler_form(self, alquiler_id, **kwargs):
+        alquiler = request.env['cotizacion.alquiler'].sudo().browse(alquiler_id)
+        return request.render('copier_company.portal_alquiler_form', {'alquiler': alquiler})
