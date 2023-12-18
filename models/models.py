aa3c6667f0c3e7c8aa78e3f0927cb9c2d9f99ab8
isidrovera <118472736+isidrovera@users.noreply.github.com>
2023-12-18 17:04:20 -0500

    Update models.py

--- models/models.py
+++ models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _logger
 
 
 class copier_company(models.Model):
@@ -15,12 +15,14 @@ class copier_company(models.Model):
     cliente_id = fields.Many2one('res.partner',string='Cliente', required=True, )
     
     def name_get(self):
+        _logger.info('name_get method called for copier.company')
         result = []
         for record in self:
-            # Suponiendo que 'name' es un campo Many2one y el modelo relacionado tiene un campo 'name'.
-            maquina_name = record.name.display_name if record.name else ''
+            maquina_name = record.name.name if record.name else ''
             serie_id = record.serie_id or ''
-            result.append((record.id, f"[{maquina_name}] Serie: {serie_id}"))
+            full_name = f"[{maquina_name}] Serie: {serie_id}"
+            result.append((record.id, full_name))
+            _logger.info(f"Generated name_get for copier.company record {record.id}: {full_name}")
         return result
 
     
