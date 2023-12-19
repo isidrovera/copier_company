79503b7e256c6e37a98d4bf84d5f43a3786b6ad3
isidrovera <118472736+isidrovera@users.noreply.github.com>
2023-12-19 16:41:50 -0500

    ...

--- models/ticket.py
+++ models/ticket.py
@@ -5,6 +5,13 @@ class TicketCopier(models.Model):
     _inherit = 'helpdesk.ticket'
     
     producto_id = fields.Many2one('copier.company', string='Maquina')
+    serie_id = fields.Char(
+    related='producto_id', 
+    string='Serie'
+    
+    readonly=True,
+    store=True
+    )
     
 
     
