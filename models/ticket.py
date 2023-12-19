e5849a5412058a2f352354fecdc33d4ac362c87d
isidrovera <118472736+isidrovera@users.noreply.github.com>
2023-12-19 12:22:38 -0500

    ....

--- models/ticket.py
+++ models/ticket.py
@@ -1,10 +1,15 @@
-from odoo import models, fields, api, _
-
+from odoo import models, fields, api
 
 class TicketCopier(models.Model):
     _inherit = 'helpdesk.ticket'
     
-    producto_id = fields.Many2one('copier.company', string='Maquina')
-    
+    producto_id = fields.Many2one('copier.company', string='Máquina')
+    producto_display_name = fields.Char(string='Nombre de Máquina', compute='_compute_producto_display_name')
 
-    
+    @api.depends('producto_id')
+    def _compute_producto_display_name(self):
+        for record in self:
+            if record.producto_id:
+                record.producto_display_name = f"{record.producto_id.name} Serie: {record.producto_id.serie_id}"
+            else:
+                record.producto_display_name = ''
