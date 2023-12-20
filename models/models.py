a1d0c705426b8a3696a09dd213e34ced979a5812
isidrovera <118472736+isidrovera@users.noreply.github.com>
2023-12-20 09:00:37 -0500

    Update models.py

--- models/models.py
+++ models/models.py
@@ -15,15 +15,22 @@ class copier_company(models.Model):
     marca_id = fields.Many2one('marcas.maquinas',string='Marca', required=True,related='name.marca_id')
     cliente_id = fields.Many2one('res.partner',string='Cliente', required=True, )
     
-    _rec_name = 'serie_id'
+    full_name = fields.Char(string='Nombre Completo', compute='_compute_full_name', store=True)
+
+    @api.depends('name.name', 'serie_id')
+    def _compute_full_name(self):
+        for record in self:
+            # Concatenación del nombre del modelo y el número de serie
+            modelo_maquina = record.name.name if record.name else ''
+            serie = record.serie_id or ''
+            record.full_name = '{} Serie: {}'.format(modelo_maquina, serie)
 
     def name_get(self):
         result = []
         for record in self:
-            name = '[  ' + record.name.name + '  ]   ' + \
-                'Serie: ' + record.serie_id
-            result.append((record.id, name))
-        return result 
+            # Se utiliza el campo computado full_name para la representación del nombre
+            result.append((record.id, record.full_name))
+        return result
 
     
     def crear_ticket(self):
