1c71fa1810554a977f54940ca35638b5dcd1c5b7
isidrovera <118472736+isidrovera@users.noreply.github.com>
2023-12-20 10:47:15 -0500

    ...

--- models/ticket.py
+++ models/ticket.py
@@ -4,10 +4,10 @@ from odoo import models, fields, api, _
 class TicketCopier(models.Model):
     _inherit = 'helpdesk.ticket'
     
-    producto_id = fields.Many2one('copier.company', string='Maquina')
+    producto_id = fields.Many2one('copier.company', string='Serie')
     serie_id = fields.Char(
     related='producto_id.name.name', 
-    string='Serie',
+    string='Modelo',
     
     readonly=True,
  
