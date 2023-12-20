121299925b7a029e1a181908629797840e7ec32b
isidrovera <118472736+isidrovera@users.noreply.github.com>
2023-12-20 10:43:49 -0500

    ...

--- models/ticket.py
+++ models/ticket.py
@@ -6,22 +6,10 @@ class TicketCopier(models.Model):
     
     producto_id = fields.Many2one('copier.company', string='Maquina')
     serie_id = fields.Char(
-    related='producto_id.serie_id', 
+    related='producto_id.name.name', 
     string='Serie',
     
     readonly=True,
  
     )
     
-
-    def name_get(self):
-        result = []
-        for record in self:
-            # Esto comprueba si el ticket tiene una máquina asociada y utiliza su full_name
-            if record.producto_id:
-                name = record.producto_id.full_name
-            else:
-                # Si no hay una máquina asociada, utiliza el nombre estándar del ticket
-                name = super(TicketCopier, self).name_get()[0][1]
-            result.append((record.id, name))
-        return result
