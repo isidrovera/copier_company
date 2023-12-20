43eceec8bc3c36156d6789ebd8838b19593f297b
isidrovera <118472736+isidrovera@users.noreply.github.com>
2023-12-20 09:58:25 -0500

    Update ticket.py

--- models/ticket.py
+++ models/ticket.py
@@ -14,4 +14,14 @@ class TicketCopier(models.Model):
     )
     
 
-    
+    def name_get(self):
+        result = []
+        for record in self:
+            # Esto comprueba si el ticket tiene una máquina asociada y utiliza su full_name
+            if record.producto_id:
+                name = record.producto_id.full_name
+            else:
+                # Si no hay una máquina asociada, utiliza el nombre estándar del ticket
+                name = super(TicketCopier, self).name_get()[0][1]
+            result.append((record.id, name))
+        return result
