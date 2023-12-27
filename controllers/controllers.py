36abdb4fb37e965072eec1fa57473c95b6e19fe0
isidrovera <118472736+isidrovera@users.noreply.github.com>
2023-12-27 15:23:05 -0500

    Update controllers.py

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -4,26 +4,33 @@ from odoo.http import request
 class DescargaArchivosController(http.Controller):
     @http.route('/descarga/archivos', type='http', website=True)
     def descarga_archivos(self, page=1, search='', **kw):
-        # Asegúrate de que 'page' sea un entero
+        # Convertir 'page' a un entero
         try:
             page = int(page)
         except ValueError:
-            page = 1
+            page = 1  # En caso de error, reiniciar a la primera página
 
+        partner = request.env.user.partner_id
         items_per_page = 20
 
-        # Si hay término de búsqueda, reiniciar la paginación a la primera página
-        if search:
-            page = 1
+        # Verificar suscripciones activas
+        subscriptions_in_progress = request.env['sale.order'].sudo().search([
+            ('partner_id', '=', partner.id),
+            ('subscription_state', '=', '3_progress')
+        ])
 
-        # Definir el dominio de búsqueda basado en el término de búsqueda
-        domain = []
-        if search:
-            domain += ['|', '|',
-                       ('name', 'ilike', '%' + search + '%'),
-                       ('observacion', 'ilike', '%' + search + '%'),
-                       ('modelo.name', 'ilike', '%' + search + '%')]
-
+        if subscriptions_in_progress:
+            domain = []
+            if search:
+                domain = ['|', '|',
+                          ('name', 'ilike', '%' + search + '%'),
+                          ('observacion', 'ilike', '%' + search + '%'),
+                          ('modelo.name', 'ilike', '%' + search + '%')]
+            
+            # Si hay término de búsqueda, reiniciar la paginación a la primera página
+            if search:
+                page = 1
+            
             total_docs = request.env['descarga.archivos'].sudo().search_count(domain)
             total_pages = ((total_docs - 1) // items_per_page) + 1
             page = max(min(page, total_pages), 1)
@@ -39,9 +46,8 @@ class DescargaArchivosController(http.Controller):
                 'total_pages': total_pages,
                 'search': search
             })
-
         else:
-            # Mostrar mensaje si no hay suscripciones activas
+            # Mostrar mensaje de suscripción expirada
             return request.render('copier_company.no_subscription_message')
 
 
