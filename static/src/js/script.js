ead413403e40a2790f78dfbfc9c7122d1bd02ead
isidrovera <118472736+isidrovera@users.noreply.github.com>
2023-12-27 15:29:58 -0500

    Update script.js

--- static/src/js/script.js
+++ static/src/js/script.js
@@ -1,7 +1,12 @@
+let debounceTimeout = null;
+
 function searchFiles() {
-    var input = document.getElementById("search");
-    var filter = input.value;
-    var url = '/descarga/archivos?page=1&search=' + encodeURIComponent(filter);
-    console.log(url); // Agrega esto para depuración
-    window.location = url;
+    clearTimeout(debounceTimeout); // Cancela cualquier timeout anterior
+    debounceTimeout = setTimeout(function () {
+        var input = document.getElementById("search");
+        var filter = input.value;
+        var url = '/descarga/archivos?page=1&search=' + encodeURIComponent(filter);
+        console.log(url); // Para depuración
+        window.location = url;
+    }, 500); // Espera 500 ms después de la última tecla presionada para iniciar la búsqueda
 }
