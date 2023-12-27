f1bfb092f3683615f7b04a5bcfa40cf983ff5412
isidrovera <118472736+isidrovera@users.noreply.github.com>
2023-12-27 16:05:28 -0500

    ...

--- static/src/js/script.js
+++ static/src/js/script.js
@@ -1,10 +1,9 @@
 function searchFiles() {
     var input = document.getElementById("search");
     var filter = input.value;
-    // Solo realiza la búsqueda si hay un término para buscar.
+    // Realiza la búsqueda solo si hay un término para buscar.
     if (filter.trim().length > 0) {
         var url = '/descarga/archivos?page=1&search=' + encodeURIComponent(filter.trim());
-        console.log(url); // Para depuración
-        window.location.href = url; // Asegúrate de usar window.location.href
+        window.location.href = url;
     }
 }
