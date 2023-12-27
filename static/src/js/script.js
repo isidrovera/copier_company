154b5e58ae3180579b8aa84bb5b0882479dc2286
isidrovera <118472736+isidrovera@users.noreply.github.com>
2023-12-27 15:41:18 -0500

    ..

--- static/src/js/script.js
+++ static/src/js/script.js
@@ -1,17 +1,9 @@
-let debounceTimeout = null;
-
 function searchFiles() {
-    if (debounceTimeout) {
-        clearTimeout(debounceTimeout);
+    var input = document.getElementById("search");
+    var filter = input.value;
+    if (filter.trim()) { // Asegúrate de que la cadena no esté vacía
+        var url = '/descarga/archivos?page=1&search=' + encodeURIComponent(filter.trim());
+        console.log(url); // Para depuración
+        window.location = url;
     }
-    
-    debounceTimeout = setTimeout(function() {
-        var input = document.getElementById("search");
-        var filter = input.value;
-        if (filter.length > 1) { // Asegúrate de tener al menos 2 caracteres antes de buscar
-            var url = '/descarga/archivos?page=1&search=' + encodeURIComponent(filter);
-            console.log(url); // Para depuración
-            window.location = url;
-        }
-    }, 500); // Espera 500 ms después de que el usuario haya dejado de escribir
 }
