e367a1ed6e1c062c5304d73b4c65c8a9afa72235
isidrovera <118472736+isidrovera@users.noreply.github.com>
2024-01-05 09:07:07 -0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -47,7 +47,7 @@
         'views/vista_nube.xml',
         'views/portal_alquiler.xml',
         'views/confirmacion_formulario.xml',
-        #'views/formulario_help.xml',
+        'views/formulario_help.xml',
         #'views/assets.xml',
         #'views/add_custom_selection_field.xml',
 
