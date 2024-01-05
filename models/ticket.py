ae09835380578f157b7865bbe401b572aaac8f50
isidrovera <118472736+isidrovera@users.noreply.github.com>
2024-01-05 16:10:37 -0500

    ...

--- models/ticket.py
+++ models/ticket.py
@@ -5,7 +5,7 @@ class TicketCopier(models.Model):
     _inherit = 'helpdesk.ticket'
     
     producto_id = fields.Many2one('copier.company', string='Maquina')
-    serie_id = fields.Char(related='producto_id.name.name', string='Serie', readonly=True,)
+    serie_id = fields.Char(related='producto_id.serie_id', string='Serie', readonly=True,)
     image = fields.Binary("Imagen", attachment=True, help="Imagen relacionada con el ticket.")
     nombre_reporta  = fields.Char(
     string='Nombre de quien reporto'
