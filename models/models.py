f024f1a668c664ab54b819856ed1b5b1d003dca9
isidrovera <118472736+isidrovera@users.noreply.github.com>
2024-01-22 17:05:25 -0500

    Update models.py

--- models/models.py
+++ models/models.py
@@ -48,37 +48,34 @@ class CopierCompany(models.Model):
     qr_code = fields.Binary(string='Código QR', readonly=True)
     def generar_qr_code(self):
         base_url = "https://copiercompanysac.com//public/helpdesk_ticket"
+        # Asumiendo una impresión de 300 DPI, calcula el box_size para un tamaño de 1.5 pulgadas
+        # Para una cuarta parte de ese tamaño, dividimos por 4
+        dpi = 300
+        inches_for_qr = 1.5 / 4  # Una cuarta parte de 1.5 pulgadas
+        pixels_for_qr = int(dpi * inches_for_qr)
         
-        # Configuración de tamaño
-        dpi = 300  # puntos por pulgada
-        size_in_inches = 1.5  # tamaño deseado en pulgadas
-        total_size_in_pixels = size_in_inches * dpi
-
-        # Establecer bordes y calcular el tamaño de cada caja
-        border = 4
-        num_boxes_per_side = 21 + (2 * border)  # 21 cajas para la versión 1 del QR más los bordes
-        box_size = (total_size_in_pixels - (2 * border * dpi / 25.4)) // num_boxes_per_side
+        # El tamaño del código QR debe ser suficiente para que quepan todos los datos
+        # La versión 1 de QR (21x21) podría ser muy pequeña para algunos datos
+        # Puede que necesites ajustar la versión para que quepa tu URL
+        version_size = 21  # Tamaño de la versión 1 de QR
+        box_size = max(1, pixels_for_qr // (version_size + (2 * 4)))  # Asegurarse de que box_size no sea 0
 
         for record in self:
             data_to_encode = f"{base_url}?copier_company_id={record.id}"
 
-            # Generar el código QR
             qr = qrcode.QRCode(
-                version=1,
+                version=1,  # Puede que necesites aumentar esto si el QR no genera correctamente
                 error_correction=qrcode.constants.ERROR_CORRECT_L,
                 box_size=box_size,
-                border=border,
+                border=4,  # Un borde típico
             )
             qr.add_data(data_to_encode)
             qr.make(fit=True)
 
-            # Crear la imagen del código QR
             img = qr.make_image(fill_color="black", back_color="white")
 
-            # Convertir la imagen a una cadena de bytes y almacenarla
             img_byte_array = io.BytesIO()
             img.save(img_byte_array, format='PNG')
             qr_image_base64 = base64.b64encode(img_byte_array.getvalue()).decode('utf-8')
 
-            # Almacenar la imagen en el campo qr_code del registro
             record.qr_code = qr_image_base64
