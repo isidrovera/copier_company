66bfd1c21e05c2ed1f9c7f84d7b602bf403b7d76
isidrovera <118472736+isidrovera@users.noreply.github.com>
2024-03-03 18:49:43 -0500

    Update cloud.py

--- controllers/cloud.py
+++ controllers/cloud.py
@@ -2,6 +2,7 @@ from odoo import http
 from odoo.http import request
 import os
 import mimetypes
+from werkzeug.utils import redirect
 
 class CloudStorageController(http.Controller):
     @http.route(['/cloud/storage', '/cloud/storage/<path:extra>'], auth='user', website=True)
@@ -18,5 +19,17 @@ class CloudStorageController(http.Controller):
             'current_path': extra,
         })
     
-    # Aquí puedes agregar métodos adicionales para subir y descargar archivos
+    @http.route('/cloud/storage/download/<path:filename>', auth='user')
+    def download_file(self, filename):
+        base_path = '/mnt/cloud'
+        file_path = os.path.join(base_path, filename)
+        if os.path.exists(file_path) and os.path.isfile(file_path):
+            with open(file_path, 'rb') as f:
+                file_content = f.read()
+            return request.make_response(file_content, [
+                ('Content-Type', mimetypes.guess_type(file_path)[0] or 'application/octet-stream'),
+                ('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(file_path))
+            ])
+        return request.not_found()
+
 
