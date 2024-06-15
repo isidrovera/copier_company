6520fdf4eb2c29017ce52c3c3e05a07bafe66779
isidrovera <118472736+isidrovera@users.noreply.github.com>
2024-06-15 11:11:59 -0500

    Update backup_data.py

--- models/backup_data.py
+++ models/backup_data.py
@@ -30,23 +30,22 @@ class BackupData(models.Model):
         odoo_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
         db_name = self.env.cr.dbname
         username = self.env.user.login
-        password = self.env['backup.config.settings'].search([], limit=1).odoo_password
+        backup_config = self.env['backup.config.settings'].search([], limit=1)
+        password = backup_config.odoo_password
 
-        # Conexión a Odoo
-        odoo = odoo(odoo_url)
-        odoo.authenticate(db_name, username, password)
-
-        # Generar la copia de seguridad
-        backup_file = f"{db_name}_backup_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.zip"
+        # Generar la copia de seguridad utilizando pg_dump
+        backup_file = f"{db_name}_backup_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.dump"
         backup_path = f"/ruta/a/copias/seguridad/{backup_file}"
+        dump_cmd = f"pg_dump -Fc -h localhost -U {username} {db_name} -f {backup_path}"
 
         try:
-            odoo.db.dump(backup_file)
+            result = subprocess.run(dump_cmd, shell=True, check=True, text=True, capture_output=True)
+            _logger.info(f"Backup command output: {result.stdout}")
 
             # Subir la copia de seguridad a pCloud
             pcloud_token = pcloud_config.access_token
             pcloud_url = f"{pcloud_config.hostname}/uploadfile"
-            folder_id = self.env['backup.config.settings'].search([], limit=1).pcloud_folder_id
+            folder_id = backup_config.pcloud_folder_id
 
             with open(backup_path, 'rb') as file:
                 response = requests.post(
@@ -64,15 +63,16 @@ class BackupData(models.Model):
 
             # Eliminar la copia de seguridad local
             os.remove(backup_path)
-        except Exception as e:
+        except subprocess.CalledProcessError as e:
             self.write({'status': 'failed'})
             _logger.error(f"Backup creation failed: {e}")
             raise e
 
     @api.model
     def update_cron_frequency(self):
-        cron_frequency = self.env['backup.config.settings'].search([], limit=1).cron_frequency
-        cron = self.env.ref('copier_company.ir_cron_backup')
+        backup_config = self.env['backup.config.settings'].search([], limit=1)
+        cron_frequency = backup_config.cron_frequency
+        cron = self.env.ref('my_backup_module.ir_cron_backup')
         
         if cron_frequency == 'minutes':
             cron.write({'interval_number': 1, 'interval_type': 'minutes'})
