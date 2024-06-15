94f11a19dac3e029fed39484006b5e2c5adf0b2c
isidrovera <118472736+isidrovera@users.noreply.github.com>
2024-06-15 11:52:53 -0500

    ...

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -1,5 +1,6 @@
 from odoo import models, fields
-
+import requests
+import subprocess
 class BackupConfigSettings(models.Model):
     _name = 'backup.config.settings'
     _description = 'Backup Config Settings'
@@ -12,3 +13,29 @@ class BackupConfigSettings(models.Model):
         ('hours', 'Hours'),
         ('days', 'Days'),
     ], string="Cron Frequency", default='days', required=True)
+    def test_db_connection(self):
+        db_name = self.env.cr.dbname
+        username = self.env.user.login
+        password = self.odoo_password
+
+        test_cmd = f"pg_isready -h localhost -U {username} -d {db_name}"
+
+        try:
+            result = subprocess.run(test_cmd, shell=True, check=True, text=True, capture_output=True)
+            if result.returncode == 0:
+                message = "Database connection is successful!"
+            else:
+                message = f"Database connection failed! Error: {result.stderr}"
+        except subprocess.CalledProcessError as e:
+            message = f"Database connection test failed! Error: {e.stderr}"
+
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': ('Database Connection Test'),
+                'message': message,
+                'type': 'success' if result.returncode == 0 else 'danger',
+                'sticky': False,
+            }
+        }
