f7f1684a8b5856c0315ab6d575d8fa243db7253a
isidrovera <118472736+isidrovera@users.noreply.github.com>
2024-06-20 09:04:34 -0500

    Update PcloudPublico.py

--- controllers/PcloudPublico.py
+++ controllers/PcloudPublico.py
@@ -1,12 +1,14 @@
 from odoo import http
 from odoo.http import request
+import requests
 import logging
-import datetime
+from datetime import datetime
+
 _logger = logging.getLogger(__name__)
 
 class PdfViewerController(http.Controller):
 
-    @http.route('/pdf/viewer', type='http', auth='public', website=True)
+    @http.route('/pdf/viewer', type='http', auth='public')
     def pdf_viewer(self, file_id):
         config = request.env['pcloud.config'].search([], limit=1)
         if not config:
