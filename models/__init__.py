7f88bf70e1a214f87c8cd08108ea4d8b70491317
isidrovera <118472736+isidrovera@users.noreply.github.com>
2024-07-05 16:49:34 -0500

    ...

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,7 @@ from . import models
 from . import ticket
 from . import descargas
 from . import modelos
+from . import cotizaciones
 from . import pCloudModel
 from . import pcloud_folder_file
 
---
+++ models/cotizaciones.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+import requests
+import socket
+from odoo.exceptions import UserError
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class CotizacionAlquiler(models.Model):
+    _name = 'cotizacion.alquiler'
+    _inherit = ['mail.thread']
+    _description = 'En este formulario se crearan cotizaciones de alquiler'
+
+    marca_id = fields.Many2one('marcas.maquinas', string='Marca', help='Indique que marca en especifica esta interesado')
+    tipo = fields.Selection(string='Tipo de impresora', selection=[('monocroma', 'Blanco y negro'), ('color', 'Color')],
+                            default='monocroma', help='Aqui elija que tipo de equipo multifuncional necesita si blanco y negro o color.')
+    cantidad = fields.Char(string='Cantidad mensual', help='Indique la cantidad que realiza mensual, copias + impresión')
+    name = fields.Char('Formulario N°', default='New', copy=False, required=True, readonly=True)
+    empresa = fields.Many2one('res.partner', string='Nombre de empresa')
+    contacto = fields.Char(string='Contacto')
+    celular = fields.Char(string='Telefono')
+    correo = fields.Char(string='Correo')
+    detalles = fields.Text(string='Detalle')
+    formato = fields.Selection(string='Formato', selection=[('a4', 'A4'), ('a3', 'A3')], default='a3', help='Elija el formato de papel')
+    identificacion_type = fields.Selection([
+        ('ruc', 'RUC'),
+        ('dni', 'DNI')
+    ], string="Tipo de identificación", required=True)
+    identificacion_number = fields.Char(string='Número de Identificación', required=True)
+
+    @api.model
+    def create(self, vals):
+        vals['name'] = self.env['ir.sequence'].next_by_code('cotizacion.alquiler') or '/'
+        return super(CotizacionAlquiler, self).create(vals)
+
+    @api.onchange('identificacion_type', 'identificacion_number')
+    def _onchange_identificacion(self):
+        if self.identificacion_type and self.identificacion_number:
+            if self.identificacion_type == 'ruc':
+                self._fetch_data_from_sunat()
+            elif self.identificacion_type == 'dni':
+                self._fetch_data_from_reniec()
+
+    def _verify_dns_resolution(self, url):
+        try:
+            host = url.split('//')[1].split('/')[0]
+            _logger.info(f"Resolving DNS for host: {host}")
+            socket.gethostbyname(host)
+            return True
+        except socket.error as e:
+            _logger.error(f"DNS resolution error for {host}: {e}")
+            raise UserError(_('Error de resolución DNS para %s. Por favor, verifica tu configuración de DNS.') % host)
+
+    def _fetch_data_from_sunat(self):
+        if not self.identificacion_number:
+            return
+        if len(self.identificacion_number) != 11:
+            raise UserError(_('El número de RUC debe tener 11 dígitos.'))
+        
+        sunat_url = f"https://api.sunat.cloud/ruc/{self.identificacion_number}"
+        self._verify_dns_resolution(sunat_url)
+        
+        try:
+            response = requests.get(sunat_url)
+            if response.status_code == 200:
+                data = response.json()
+                if 'razonSocial' in data:
+                    existing_partner = self.env['res.partner'].search([('vat', '=', self.identificacion_number)], limit=1)
+                    if existing_partner:
+                        self.empresa = existing_partner.id
+                        return {
+                            'warning': {
+                                'title': "Cliente existente",
+                                'message': "El cliente ya existe en el sistema.",
+                            }
+                        }
+                    else:
+                        partner_vals = {
+                            'name': data['razonSocial'],
+                            'street': data.get('direccion', ''),
+                            'vat': self.identificacion_number,
+                        }
+                        partner = self.env['res.partner'].create(partner_vals)
+                        self.empresa = partner.id
+                        self.contacto = data.get('nombre', '')
+                else:
+                    _logger.warning(f"Datos no encontrados para el RUC: {self.identificacion_number}")
+                    raise UserError(_('Datos no encontrados para el RUC proporcionado.'))
+            else:
+                _logger.error(f"Error al consultar el RUC en SUNAT: {response.text}")
+                raise UserError(_('Error al consultar el RUC en SUNAT.'))
+        except requests.exceptions.RequestException as e:
+            _logger.error(f"Error fetching data from SUNAT: {e}")
+            raise UserError(_('Error al consultar el RUC en SUNAT.'))
+
+    def _fetch_data_from_reniec(self):
+        if not self.identificacion_number:
+            return
+        if len(self.identificacion_number) != 8:
+            raise UserError(_('El número de DNI debe tener 8 dígitos.'))
+        
+        reniec_url = f"https://api.apis.net.pe/v1/dni?numero={self.identificacion_number}"
+        self._verify_dns_resolution(reniec_url)
+        
+        try:
+            response = requests.get(reniec_url)
+            if response.status_code == 200:
+                data = response.json()
+                if 'nombres' in data and 'apellidoPaterno' in data and 'apellidoMaterno' in data:
+                    existing_partner = self.env['res.partner'].search([('vat', '=', self.identificacion_number)], limit=1)
+                    if existing_partner:
+                        self.empresa = existing_partner.id
+                        return {
+                            'warning': {
+                                'title': "Cliente existente",
+                                'message': "El cliente ya existe en el sistema.",
+                            }
+                        }
+                    else:
+                        partner_vals = {
+                            'name': f"{data['nombres']} {data['apellidoPaterno']} {data['apellidoMaterno']}",
+                            'vat': self.identificacion_number,
+                        }
+                        partner = self.env['res.partner'].create(partner_vals)
+                        self.empresa = partner.id
+                        self.contacto = f"{data['nombres']} {data['apellidoPaterno']} {data['apellidoMaterno']}"
+                else:
+                    _logger.warning(f"Datos no encontrados para el DNI: {self.identificacion_number}")
+                    raise UserError(_('Datos no encontrados para el DNI proporcionado.'))
+            else:
+                _logger.error(f"Error al consultar el DNI en RENIEC: {response.text}")
+                raise UserError(_('Error al consultar el DNI en RENIEC.'))
+        except requests.exceptions.RequestException as e:
+            _logger.error(f"Error fetching data from RENIEC: {e}")
+            raise UserError(_('Error al consultar el DNI en RENIEC.'))
