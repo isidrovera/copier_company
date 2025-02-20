e1c53fe5bc1e02591cb306730039e125341ab7b6
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-02-20 11:35:06 -0500

    ...

--- controllers/auth_signup.py
+++ controllers/auth_signup.py
@@ -1,43 +1,41 @@
-from odoo import http
+from odoo import http, _
 from odoo.addons.auth_signup.controllers.main import AuthSignupHome
 from odoo.http import request
+import requests
 
 class ExtendedAuthSignup(AuthSignupHome):
-    
-    @http.route()
-    def web_auth_signup(self, *args, **kw):
-        providers = request.env['auth.oauth.provider'].sudo().search([('enabled', '=', True)])
-        values = {
-            'providers': providers,
-        }
-        
-        # Validar reCAPTCHA si está presente
-        if request.httprequest.method == 'POST':
-            if not self._validate_recaptcha():
-                values['error'] = 'Por favor complete el reCAPTCHA'
-                return request.render('auth_signup.signup', values)
-        
-        response = super().web_auth_signup(*args, **kw)
-        
-        # Si la respuesta es un dict o QWeb, añadir los providers
-        if isinstance(response, (dict, http.Response)):
-            if isinstance(response, dict):
-                response.update(values)
-            return response
-            
-        return response
 
-    def _validate_recaptcha(self):
+    @http.route('/web/signup', type='http', auth='public', website=True, sitemap=False)
+    def web_signup(self, *args, **kw):
+        if request.httprequest.method == 'POST':
+            # Verificar reCAPTCHA
+            if not self._verify_recaptcha():
+                qcontext = self.get_auth_signup_qcontext()
+                qcontext['error'] = _("Por favor complete el reCAPTCHA")
+                response = request.render('auth_signup.signup', qcontext)
+                return response
+
+            # Verificar términos y condiciones
+            if not kw.get('terms'):
+                qcontext = self.get_auth_signup_qcontext()
+                qcontext['error'] = _("Debe aceptar los términos y condiciones")
+                response = request.render('auth_signup.signup', qcontext)
+                return response
+
+        return super(ExtendedAuthSignup, self).web_signup(*args, **kw)
+
+    def _verify_recaptcha(self):
         recaptcha_response = request.params.get('g-recaptcha-response')
         if not recaptcha_response:
             return False
-        
+
         try:
             secret_key = request.env['ir.config_parameter'].sudo().get_param('recaptcha.secret_key')
-            r = requests.post('https://www.google.com/recaptcha/api/siteverify', data={
+            response = requests.post('https://www.google.com/recaptcha/api.siteverify', {
                 'secret': secret_key,
-                'response': recaptcha_response
+                'response': recaptcha_response,
             })
-            return r.json().get('success', False)
-        except:
+            result = response.json()
+            return result.get('success', False)
+        except Exception:
             return False
--- static/src/js/signup_validation.js
+++ static/src/js/signup_validation.js
@@ -1,10 +1,22 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, onWillStart, onMounted } from "@odoo/owl";
 
 export class SignupValidation extends Component {
     setup() {
-        this.setupEmailValidation();
+        onWillStart(() => this.loadRecaptcha());
+        onMounted(() => {
+            this.setupEmailValidation();
+            this.setupFormValidation();
+        });
+    }
+
+    async loadRecaptcha() {
+        const script = document.createElement('script');
+        script.src = 'https://www.google.com/recaptcha/api.js';
+        script.async = true;
+        script.defer = true;
+        document.head.appendChild(script);
     }
 
     setupEmailValidation() {
@@ -12,6 +24,15 @@ export class SignupValidation extends Component {
         if (loginInput) {
             loginInput.addEventListener('input', this.validateEmail);
         }
+    }
+
+    setupFormValidation() {
+        document.addEventListener('DOMContentLoaded', () => {
+            const submitButton = document.querySelector('button[type="submit"]');
+            if (submitButton) {
+                submitButton.disabled = true;
+            }
+        });
     }
 
     validateEmail(event) {
@@ -25,3 +46,19 @@ export class SignupValidation extends Component {
         }
     }
 }
+
+// Función global para reCAPTCHA callback
+window.enableSignupSubmit = function(token) {
+    const submitButton = document.querySelector('button[type="submit"]');
+    if (submitButton && token) {
+        submitButton.disabled = false;
+    }
+};
+
+// Registrar el componente
+odoo.define('auth_signup.SignupValidation', function (require) {
+    'use strict';
+    
+    const { registry } = require('@web/core/registry');
+    registry.category('public_components').add('signup_validation', SignupValidation);
+});
