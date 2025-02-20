b57e1d231484171ec86ad8a80a5468304be7117b
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-02-20 11:27:56 -0500

    ..

--- static/src/js/signup_validation.js
+++ static/src/js/signup_validation.js
@@ -1,24 +1,16 @@
-/** @odoo-module */
+/** @odoo-module **/
 
-import { loadJS } from "@web/core/assets";
-
-const RECAPTCHA_URL = 'https://www.google.com/recaptcha/api.js';
+import { Component } from "@odoo/owl";
 
 export class SignupValidation extends Component {
     setup() {
-        this.loadRecaptcha();
         this.setupEmailValidation();
-        this.disableSubmitButton();
-    }
-
-    async loadRecaptcha() {
-        await loadJS(RECAPTCHA_URL);
     }
 
     setupEmailValidation() {
         const loginInput = document.querySelector('#login');
         if (loginInput) {
-            loginInput.addEventListener('input', this.validateEmail.bind(this));
+            loginInput.addEventListener('input', this.validateEmail);
         }
     }
 
@@ -32,19 +24,4 @@ export class SignupValidation extends Component {
             event.target.setCustomValidity('');
         }
     }
-
-    disableSubmitButton() {
-        const submitButton = document.querySelector('.oe_signup_form button[type="submit"]');
-        if (submitButton) {
-            submitButton.disabled = true;
-        }
-    }
 }
-
-// Función global para reCAPTCHA
-window.enableSignupSubmit = function(token) {
-    const submitButton = document.querySelector('.oe_signup_form button[type="submit"]');
-    if (submitButton) {
-        submitButton.disabled = false;
-    }
-};
