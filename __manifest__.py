fd884a1f5ac99029e6cb83ce2c3a22d1323700da
isidrovera <118472736+isidrovera@users.noreply.github.com>
2025-04-23 17:15:18 -0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -92,8 +92,7 @@
             'copier_company/static/src/js/counter_charts.js',
             'copier_company/static/src/js/PcloudDescargas.js',
             'copier_company/static/src/js/custom_script.js',
-            'copier_company/static/src/js/copier_list.js',
-            # 4) Bibliotecas externas si las necesitas
+                        # 4) Bibliotecas externas si las necesitas
             'copier_company/static/lib/pdfjs/pdf.mjs',
             'copier_company/static/lib/pdfjs/pdf.worker.mjs',
             'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js',
