7705842512ec1b7f80b1a01cf7b3a227c8a01817
isidrovera <118472736+isidrovera@users.noreply.github.com>
2026-03-07 08:55:15 -0500

    ...

--- static/src/js/script.js
+++
@@ -1,9 +0,0 @@
-function searchFiles() {
-    var input = document.getElementById("search");
-    var filter = input.value;
-    // Realiza la búsqueda solo si hay un término para buscar.
-    if (filter.trim().length > 0) {
-        var url = '/descarga/archivos?page=1&search=' + encodeURIComponent(filter.trim());
-        window.location.href = url;
-    }
-}
